abc3598e9ed6a2ba828196bbb50f5e929e188cdc
lisindima <lisindima1996@gmail.com>
2020-08-17 11:14:13 +0700

    swiftformat

--- Package.swift
+++ Package.swift
@@ -7,13 +7,14 @@ let package = Package(
     name: "NativeSearchBar",
     platforms: [
         .iOS(.v14),
-        .watchOS(.v7)
+        .watchOS(.v7),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NativeSearchBar",
-            targets: ["NativeSearchBar"]),
+            targets: ["NativeSearchBar"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,6 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NativeSearchBar",
-            dependencies: []),
+            dependencies: []
+        ),
     ]
 )
--- Sources/NativeSearchBar/NativeSearchBar.swift
+++ Sources/NativeSearchBar/NativeSearchBar.swift
@@ -6,98 +6,92 @@
 //  Copyright © 2020 Дмитрий Лисин. All rights reserved.
 //
 
+import Combine
 import Foundation
 import SwiftUI
-import Combine
 
 public class SearchBar: NSObject, ObservableObject {
-    
     @Published public var text: String = ""
-    
+
     public static let shared = SearchBar()
-    
+
     #if !os(watchOS)
-    public let searchController: UISearchController = UISearchController(searchResultsController: nil)
-    
-    public override init() {
-        super.init()
-        self.searchController.obscuresBackgroundDuringPresentation = false
-        self.searchController.searchResultsUpdater = self
-    }
+        public let searchController: UISearchController = UISearchController(searchResultsController: nil)
+
+        override public init() {
+            super.init()
+            searchController.obscuresBackgroundDuringPresentation = false
+            searchController.searchResultsUpdater = self
+        }
     #endif
 }
 
 #if !os(watchOS)
-extension SearchBar: UISearchResultsUpdating {
-    public func updateSearchResults(for searchController: UISearchController) {
-        if let searchBarText = searchController.searchBar.text {
-            self.text = searchBarText
+    extension SearchBar: UISearchResultsUpdating {
+        public func updateSearchResults(for searchController: UISearchController) {
+            if let searchBarText = searchController.searchBar.text {
+                text = searchBarText
+            }
         }
     }
-}
 #endif
 
 public struct SearchBarModifier: ViewModifier {
-    
     public let searchBar: SearchBar
-    
+
     @ViewBuilder
     public func body(content: Content) -> some View {
         #if os(watchOS)
-        content
+            content
         #else
-        content
-            .overlay(
-                ViewControllerResolver { viewController in
-                    viewController.navigationItem.searchController = self.searchBar.searchController
-                }.frame(width: 0, height: 0)
-        )
+            content
+                .overlay(
+                    ViewControllerResolver { viewController in
+                        viewController.navigationItem.searchController = self.searchBar.searchController
+                    }.frame(width: 0, height: 0)
+                )
         #endif
     }
 }
 
 public extension View {
     func addSearchBar(_ searchBar: SearchBar) -> some View {
-        return self.modifier(SearchBarModifier(searchBar: searchBar))
+        modifier(SearchBarModifier(searchBar: searchBar))
     }
 }
 
 #if !os(watchOS)
-public final class ViewControllerResolver: UIViewControllerRepresentable {
-    
-    public let onResolve: (UIViewController) -> Void
-    
-    public init(onResolve: @escaping (UIViewController) -> Void) {
-        self.onResolve = onResolve
-    }
-    
-    public func makeUIViewController(context: Context) -> ParentResolverViewController {
-        ParentResolverViewController(onResolve: onResolve)
-    }
-    
-    public func updateUIViewController(_ uiViewController: ParentResolverViewController, context: Context) {
-    }
-}
+    public final class ViewControllerResolver: UIViewControllerRepresentable {
+        public let onResolve: (UIViewController) -> Void
 
-public class ParentResolverViewController: UIViewController {
-    
-    public let onResolve: (UIViewController) -> Void
-    
-    public init(onResolve: @escaping (UIViewController) -> Void) {
-        self.onResolve = onResolve
-        super.init(nibName: nil, bundle: nil)
+        public init(onResolve: @escaping (UIViewController) -> Void) {
+            self.onResolve = onResolve
+        }
+
+        public func makeUIViewController(context _: Context) -> ParentResolverViewController {
+            ParentResolverViewController(onResolve: onResolve)
+        }
+
+        public func updateUIViewController(_: ParentResolverViewController, context _: Context) {}
     }
-    
-    public required init?(coder: NSCoder) {
-        fatalError("Use init(onResolve:) to instantiate ParentResolverViewController.")
-    }
-    
-    public override func didMove(toParent parent: UIViewController?) {
-        super.didMove(toParent: parent)
-        if let parent = parent {
-            onResolve(parent)
+
+    public class ParentResolverViewController: UIViewController {
+        public let onResolve: (UIViewController) -> Void
+
+        public init(onResolve: @escaping (UIViewController) -> Void) {
+            self.onResolve = onResolve
+            super.init(nibName: nil, bundle: nil)
+        }
+
+        public required init?(coder _: NSCoder) {
+            fatalError("Use init(onResolve:) to instantiate ParentResolverViewController.")
+        }
+
+        override public func didMove(toParent parent: UIViewController?) {
+            super.didMove(toParent: parent)
+            if let parent = parent {
+                onResolve(parent)
+            }
         }
     }
-}
 #endif
-
