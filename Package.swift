1669876b4760fb3b25d82cc5be60080e56a0f38f
lisindima <lisindima1996@gmail.com>
2020-09-23 09:27:41 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "NativeSearchBar",
     platforms: [
-        .iOS(.v14),
-        .watchOS(.v7),
+        .iOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
