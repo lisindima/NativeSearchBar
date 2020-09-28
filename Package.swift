f905c45f9df75c453375097a5510d5ca68489624
lisindima <lisindima1996@gmail.com>
2020-09-28 15:10:28 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .watchOS(.v6),
+        .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
